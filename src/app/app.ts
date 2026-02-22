ef5f09f4d55452396e7a8d1944127604ed8caa2d
Alex ferreira <fereiraalex233@gmail.com>
2026-02-22 11:30:43 -0300

    Creating and styling base-ui and certificate items.

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,12 @@ import { Navbar } from "./_components/navbar/navbar";
 import { CommonModule } from '@angular/common';
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { IntemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUi } from "./_components/base-ui/base-ui";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, IntemCertificado, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
