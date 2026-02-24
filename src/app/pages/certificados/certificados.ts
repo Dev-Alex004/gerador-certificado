5ca02e2f7dff453ba38e9e362bcd5f69a042f45c
Alex ferreira <fereiraalex233@gmail.com>
2026-02-24 20:45:31 -0300

    styiling certificate-form

--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
-import { IntemCertificado } from "../../_components/item-certificado/item-certificado";
+import { ItemCertificado } from "../../_components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecondaryButton, IntemCertificado],
+  imports: [ItemCertificado],
   templateUrl: './certificados.html',
   styleUrl: './certificados.css',
 })
